e4d1383b584353b191e0a53f00b62f90b654edfc
amitrke <amitrke@gmail.com>
2018-10-13 16:22:42 -0400

    Fixed post content view

--- src/app/myacc/editpost/editpost.component.ts
+++ src/app/myacc/editpost/editpost.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
 import { FileuploadService } from '../../services/fileupload.service';
 import { UserService } from '../../services/user.service';
 import { ContentEntity } from '../../entity/content.entity';
 import { ContentService } from '../../services/content.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-editpost',
   templateUrl: './editpost.component.html',
   styleUrls: ['./editpost.component.scss']
 })
-export class EditpostComponent implements OnInit {
+export class EditpostComponent implements OnInit, DoCheck {
 
-  public imageList: string[];
+  public imageList: string[] = [];
 
   constructor(
     private fileUploadService: FileuploadService,
     private userService: UserService,
-    private contentService: ContentService
+    private contentService: ContentService,
+    public snackBar: MatSnackBar
   ) { }
 
   model = new ContentEntity(undefined, undefined, undefined, undefined, 100);
@@ -24,13 +26,22 @@ export class EditpostComponent implements OnInit {
   ngOnInit() {
     if (this.userService.cachedUser != null) {
       this.model.userId = this.userService.cachedUser.id;
+      this.imageList = this.fileUploadService.imageListCache;
+    }
+  }
+
+  ngDoCheck() {
+    if (this.imageList.length !== this.fileUploadService.imageListCache.length) {
+      this.imageList = this.fileUploadService.imageListCache;
     }
   }
 
   onSubmit() {
-    console.log('Form submit');
     this.contentService.save(this.model).subscribe(data => {
-      console.log(data);
+      this.model = new ContentEntity(undefined, undefined, undefined, undefined, 100);
+      this.snackBar.open('Content uploaded', undefined, {
+        duration: 2000,
+      });
     },
     error => {
       console.log(error);
